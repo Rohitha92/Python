4b1036b45cd447dea856562c223eff8c2e302556
Rohitha92 <romatta@mix.wvu.edu>
2018-06-20 15:24:14 -0400

    Update StringCompression.py

--- Arrays/StringCompression.py
+++ Arrays/StringCompression.py
@@ -1,14 +1,4 @@
 #string compression with case sensitive
-'''
-input: aaaaAAAb output: a4A3b1
-input: aB output: a1B1
-'''
-'''
-1.read in the string. 
-2. creat letter count dictionary
-3.print the dictionary with key and values in a string
-'''
-
 
 def String_comp(a):
   if len(a) == 0:
