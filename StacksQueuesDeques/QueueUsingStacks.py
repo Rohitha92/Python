825ec921a810b9a552ca4c7e6f6beba97e441fee
Rohitha92 <romatta@mix.wvu.edu>
2018-06-20 15:31:50 -0400

    Update QueueUsingStacks.py

--- StacksQueuesDeques/QueueUsingStacks.py
+++ StacksQueuesDeques/QueueUsingStacks.py
@@ -1,14 +1,5 @@
 #Implement queue using 2 stacks.
-###Use list instead od stack class
-'''
-1. stack : Last in First out
-2. quque : First in First out
-###Reversing the stack twice will give out First in First out
-3. one stack for reading in
-4. next stack for popping
-5. add elements to inStack
-6. while popping, add from inStack to outStack and popp normally as in stack
-'''
+
 
 class Queue2Stacks(object):
   
